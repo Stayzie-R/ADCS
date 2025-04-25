b9b017ed46faec655cac7abaa2938ee655568bc8
Stayzie-R <Rosova.Stazka@gmail.com>
2025-04-25 08:34:29 +0200

    Create Threads

--- adcs.py
+++ adcs.py
@@ -1,3 +1,4 @@
+import threading
 from sun_sensor import SunSensor
 
 
@@ -23,7 +24,41 @@ class ADCS:
         }
 
     def run(self):
-        self.sensors["sun_sensor"].run()
+        """
+        Start the ADCS operation.
+
+        This method starts the Attitude Determination System (ADS) and the Attitude
+        Control System (ACS) in separate threads.
+        """
+        ads_thread = threading.Thread(target=self.attitude_determination_system)
+        ads_thread.start()
+
+        # acs_thread = threading.Thread(target=self.attitude_control_system)
+        # acs_thread.start()
+
+
+    def attitude_determination_system(self):
+        """
+        Attitude Determination System (ADS).
+
+        This method runs the Attitude Determination System (ADS) which collects data
+        from sensors to determine the orientation of the satellite.
+        """
+        sensor_threads = dict()
+        for name, sensor in self.sensors.items():
+            sensor_threads[name] = threading.Thread(target=sensor.run)
+            sensor_threads[name].start()
+
+
+    def attitude_control_system(self):
+        """
+        Attitude Control System (ACS).
+
+        This method runs the Attitude Control System (ACS) which is responsible for
+        controlling the orientation of the satellite based on the data obtained from
+        the Attitude Determination System (ADS).
+        """
+        pass
 
 if __name__ == "__main__":
     app = ADCS()
